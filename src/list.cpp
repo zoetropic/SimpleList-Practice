87df0524c8db254234c8c471efb66ae13b0517d8
zoetropic <pikachu94@gmail.com>
2026-02-03 01:12:04 -0600

    updating delete func

--- src/list.cpp
+++ src/list.cpp
@@ -52,13 +52,16 @@ void List::add_item() {
 void List::delete_item() {
 
     cout << "*** Delete Item ***\n";
-    cout << "Select an item index number to delete\n";
+    cout << "Select an item index number to delete: \n";
 
     if(list.size()) {
 
         for(unsigned int i=0; i< list.size(); i++){
             cout << i << ": " << list[i] << "\n";
             }   
+            int choiceNum;
+            cin >> choiceNum;
+            list.erase(list.begin()+choiceNum);
         }
     else {
         cout << "No items in the list or to delete.\n";
